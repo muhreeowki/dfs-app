baaf4f69676211d5ace2407e987be51729e25f00
Muriuki <97036296+muhreeowki@users.noreply.github.com>
2025-06-13 15:44:41 +0300

    clean up

--- main.go
+++ main.go
@@ -1,20 +1,17 @@
 package main
 
 import (
-	"bytes"
-	"io"
 	"log"
-	"strconv"
 	"time"
 
 	"github.com/muhreeowki/dfs/p2p"
 )
 
 // TODO:
-// 1. Add and Implement Automatic Peer Discovery
-// 2. Add and Implement Remove function
-// 3. Implement File Syncing
-// 4. Implement a Consensus Algorithm
+// Implement a way to add servers
+// Implement a client to read, write, and delete files from the system.
+// Implement some sort of security measures.
+// Research Consensus Algorithm
 
 func main() {
 	s1 := makeServer("store1", ":3000")
@@ -30,47 +27,7 @@ func main() {
 	go s3.Start()
 	time.Sleep(time.Millisecond * 1)
 
-	for i := range 10 {
-		key := "christian" + strconv.Itoa(i)
-		data := bytes.NewReader([]byte(key + ":\tI can do all things through Christ who strengthens me."))
-		if err := s2.Store(key, data, true); err != nil {
-			log.Fatal(err)
-		}
-		if err := s2.store.Delete(s2.ID, key); err != nil {
-			log.Fatal(err)
-		}
-		r, err := s2.Get(key)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		b, err := io.ReadAll(r)
-		if err != nil {
-			log.Fatal(err)
-		}
-		log.Printf("File contents for file (%s): %s", key, b)
-	}
-
-	for i := 10; i < 20; i++ {
-		key := "christian" + strconv.Itoa(i)
-		data := bytes.NewReader([]byte(key + ":\tI can do all things through Christ who strengthens me."))
-		if err := s1.Store(key, data, true); err != nil {
-			log.Fatal(err)
-		}
-		if err := s1.store.Delete(s1.ID, key); err != nil {
-			log.Fatal(err)
-		}
-		r, err := s1.Get(key)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		b, err := io.ReadAll(r)
-		if err != nil {
-			log.Fatal(err)
-		}
-		log.Printf("File contents for file (%s): %s", key, b)
-	}
+	select {}
 }
 
 func makeServer(id, listenAddr string, nodes ...string) *FileServer {
@@ -86,7 +43,7 @@ func makeServer(id, listenAddr string, nodes ...string) *FileServer {
 	tcpTransport := p2p.NewTCPTransport(tcpOpts)
 	serverOpts := FileServerOpts{
 		ID:                id,
-		Enkey:             newEncryptionKey(),
+		Encryptionkey:     newEncryptionKey(),
 		Transport:         tcpTransport,
 		PathTransformFunc: CASPathTransformFunc,
 		StorageFolder:     listenAddr[1:] + "_network",
